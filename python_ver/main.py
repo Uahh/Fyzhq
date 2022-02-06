c18abd9087d0a874c9826309508b89c4f782a5a9
Uahh <hnyrarara@gmail.com>
2022-02-07 01:08:06 +0800

    添加德语

--- python_ver/main.py
+++ python_ver/main.py
@@ -2,9 +2,10 @@ import sys
 import json
 import pypinyin
 
-sentence = pypinyin.pinyin('演的吧。演的怎么了？人生如戏，不能演吗？真搞不懂为什么有些人总是天天在下面怀疑别人演不演的，爱看不看不看拉倒！就是演的也比你们强，整天看到你们在这里怀疑这个怀疑那个，真的气的我眼泪在眼眶打转，手攥紧了衣角整个人都在颤抖，太让我失望了更多的是心寒，我想我需要退网一段时间冷静冷静，希望大家支持。', style=pypinyin.NORMAL)
+sentence = pypinyin.pinyin(
+    '演的吧。演的怎么了？人生如戏，不能演吗？真搞不懂为什么有些人总是天天在下面怀疑别人演不演的，爱看不看不看拉倒！就是演的也比你们强，整天看到你们在这里怀疑这个怀疑那个，真的气的我眼泪在眼眶打转，手攥紧了衣角整个人都在颤抖，太让我失望了更多的是心寒，我想我需要退网一段时间冷静冷静，希望大家支持。', style=pypinyin.NORMAL)
 
-with open("./data/language.json" , encoding='utf_8') as json_file:
+with open("./data/language.json", encoding='utf_8') as json_file:
     language = json.load(json_file)
     json_file.close()
 
@@ -12,7 +13,10 @@ result = ""
 
 if sys.argv[1] == "-jp":
     for word in sentence:
-        result += language["japanese"][word[0]]
+        result += language["Japanese"][word[0]]
+elif sys.argv[1] == "-gm":
+    for word in sentence:
+        result += language["German"][word[0]]
 else:
     result = "Error"
 
